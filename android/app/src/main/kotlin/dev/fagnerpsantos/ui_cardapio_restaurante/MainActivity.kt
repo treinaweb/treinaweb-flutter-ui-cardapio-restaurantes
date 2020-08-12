1d62df73bb8e65a0a53de96747a0f548dbc2ddb9
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2020-08-12 10:10:32 -0300

    02.01_Criando_Model_Produto

---
+++ android/app/src/main/kotlin/dev/fagnerpsantos/ui_cardapio_restaurante/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.fagnerpsantos.ui_cardapio_restaurante
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/produto.dart
@@ -0,0 +1,62 @@
+
+class Produto {
+  String imagem, nome, descricao;
+  int id;
+  double preco;
+
+  Produto({
+    this.id,
+    this.imagem,
+    this.nome,
+    this.descricao,
+    this.preco,
+  });
+}
+
+  List<Produto> produtos = [
+    Produto (
+      id: 1,
+      nome: "Hamburguer 1",
+      preco: 25.0,
+      descricao: desc,
+      imagem: "aqui vai a imagem depois",
+    ),
+    Produto (
+      id: 2,
+      nome: "Hamburguer 2",
+      preco: 25.0,
+      descricao: desc,
+      imagem: "aqui vai a imagem depois",
+    ),
+    Produto (
+      id: 3,
+      nome: "Hamburguer 3",
+      preco: 25.0,
+      descricao: desc,
+      imagem: "aqui vai a imagem depois",
+    ),
+    Produto (
+      id: 4,
+      nome: "Hamburguer 4",
+      preco: 25.0,
+      descricao: desc,
+      imagem: "aqui vai a imagem depois",
+    ),
+    Produto (
+      id: 5,
+      nome: "Hamburguer 5",
+      preco: 25.0,
+      descricao: desc,
+      imagem: "aqui vai a imagem depois",
+    ),
+    Produto (
+      id: 6,
+      nome: "Hamburguer 6",
+      preco: 25.0,
+      descricao: desc,
+      imagem: "aqui vai a imagem depois",
+    ),
+  ];
+
+  String desc = "Um excelente hamburguer";
+
