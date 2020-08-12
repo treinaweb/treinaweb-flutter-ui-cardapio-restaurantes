3eadaa84ce4aacc3bb9721ea677d896ad0429cca
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2020-08-12 10:17:08 -0300

    02.02_Definindo_Assets_Projeto

--- lib/models/produto.dart
+++ lib/models/produto.dart
@@ -19,44 +19,45 @@ class Produto {
       nome: "Hamburguer 1",
       preco: 25.0,
       descricao: desc,
-      imagem: "aqui vai a imagem depois",
+      imagem: caminho,
     ),
     Produto (
       id: 2,
       nome: "Hamburguer 2",
       preco: 25.0,
       descricao: desc,
-      imagem: "aqui vai a imagem depois",
+      imagem: caminho,
     ),
     Produto (
       id: 3,
       nome: "Hamburguer 3",
       preco: 25.0,
       descricao: desc,
-      imagem: "aqui vai a imagem depois",
+      imagem: caminho,
     ),
     Produto (
       id: 4,
       nome: "Hamburguer 4",
       preco: 25.0,
       descricao: desc,
-      imagem: "aqui vai a imagem depois",
+      imagem: caminho,
     ),
     Produto (
       id: 5,
       nome: "Hamburguer 5",
       preco: 25.0,
       descricao: desc,
-      imagem: "aqui vai a imagem depois",
+      imagem: caminho,
     ),
     Produto (
       id: 6,
       nome: "Hamburguer 6",
       preco: 25.0,
       descricao: desc,
-      imagem: "aqui vai a imagem depois",
+      imagem: caminho,
     ),
   ];
 
   String desc = "Um excelente hamburguer";
+  String caminho = "assets/images/burguer.png";
 
