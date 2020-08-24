56f23c2f625d43c8247c14536cdc51cbef36f574
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2020-08-24 11:01:30 -0300

    05.02_Adicionando_Itens_Drawer

--- lib/screens/home/components/drawer.dart
+++ lib/screens/home/components/drawer.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ui_cardapio_restaurante/models/produto.dart';
+import 'package:ui_cardapio_restaurante/screens/detalhes/datalhes.dart';
 
 Drawer drawer(BuildContext context) {
   return Drawer(
@@ -11,7 +13,32 @@ Drawer drawer(BuildContext context) {
           currentAccountPicture: CircleAvatar(
             child: Text("SZ"),
           ),
-        )
+        ),
+        ListTile(
+          leading: Icon(Icons.person),
+          title: Text("Minha conta"),
+          onTap: () {
+            Navigator.pop(context);
+            //Navegar para outra página
+          },
+        ),
+        ListTile(
+          leading: Icon(Icons.shopping_basket),
+          title: Text("Meus pedidos"),
+          onTap: () {
+            Navigator.pop(context);
+            //Navegar para outra página
+
+          },
+        ),
+        ListTile(
+          leading: Icon(Icons.favorite),
+          title: Text("Favoritos"),
+          onTap: () {
+            Navigator.pop(context);
+            //Navegar para outra página
+          },
+        ),
       ],
     ),
   );
